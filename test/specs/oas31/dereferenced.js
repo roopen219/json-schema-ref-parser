a2dab1d701f826198f3eb6ef1a2eb91820e399a0
Daniel Oaks <daniel@danieloaks.net>
2022-03-19 02:49:46 +1000

    fix: add OpenAPI 3.1 $ref overrides (#2)
    * Add OpenAPI 3.1  overrides
    
    * Remove old code

---
+++ test/specs/oas31/dereferenced.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports =
+{
+  required: [
+    "name"
+  ],
+  type: "object",
+  definitions: {
+    name: {
+      type: "string",
+      description: "Someone's name"
+    }
+  },
+  "properties": {
+    "name": {
+      "description": "Someone's name",
+      "type": "string"
+    },
+    "secretName": {
+      "description": "Someone's secret name",
+      "type": "string"
+    }
+  },
+  title: "Person"
+};
---
+++ test/specs/oas31/parsed.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports =
+{
+  required: [
+    "name"
+  ],
+  type: "object",
+  definitions: {
+    name: {
+      type: "string",
+      description: "Someone's name"
+    }
+  },
+  "properties": {
+    "name": {
+      "$ref": "#/definitions/name"
+    },
+    "secretName": {
+      "$ref": "#/definitions/name",
+      "description": "Someone's secret name"
+    }
+  },
+  title: "Person"
+};
